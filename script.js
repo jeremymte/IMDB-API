69c9be4c6ee2dfd5bfec3d97b6d4dee8d9744e1b
Jeremy Te <jayte718@gmail.com>
2019-10-15 18:17:06 -0400

    Created basic HTML skeleton, scripted out api call and injected information into HTML for title, and relative information

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 var apiKey = config.API_KEY;
+
+function searchMovieTitle(searchTerm) {
+    fetch(`http://www.omdbapi.com/?t=${searchTerm}&apikey=${apiKey}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+    let movieTitle = document.getElementById('movieTitle');
+    let resultsInformation = document.getElementById('resultsInformation');
+    let moviePoster = document.getElementById('moviePoster');
+
+    movieTitle.innerHTML = '<h1>' + resultFromServer.Title + '</h1>'
+    moviePoster.innerHTML = "<img class='urlImage' src='" + resultFromServer.Poster + "'>"
+    resultsInformation.innerHTML = "<div class='resultsInformation'>" + "<span class='title'>" + "Title: " + resultFromServer.Title + "</span>" + "<span class='year'>" + "Year: " + resultFromServer.Year + "</span>" + "<span class='rated'>" + "Rated: " + resultFromServer.Rated + "</span>" + "<span class='released'>" + "Released: " + resultFromServer.Released + "</span>" + "<span class='Genre'>" + "Genre: " + resultFromServer.Genre + "</span>" + "<span class='director'>" + "Director: " + resultFromServer.Director + "</span>" + "<span class='writer'>" + "Writer: " + resultFromServer.Writer + "</span>"
+}
+
+document.getElementById('searchMovieTitleButton').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm)
+        searchMovieTitle(searchTerm);
+})
