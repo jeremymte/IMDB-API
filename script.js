6b9e6c4bb293524ce41cc6f922c3ed227090a74c
Jeremy Te <jayte718@gmail.com>
2019-10-15 20:25:29 -0400

    Created for loop within script to pull ratings information from json

--- script.js
+++ script.js
@@ -12,10 +12,17 @@ function init(resultFromServer) {
     let movieTitle = document.getElementById('movieTitle');
     let resultsInformation = document.getElementById('resultsInformation');
     let moviePoster = document.getElementById('moviePoster');
+    let ratings = "<div class='ratings'>";
 
     movieTitle.innerHTML = '<h1>' + resultFromServer.Title + '</h1>'
     moviePoster.innerHTML = "<img class='urlImage' src='" + resultFromServer.Poster + "'>"
-    resultsInformation.innerHTML = "<div class='resultsInformation'>" + "<span class='title'>" + "Title: " + resultFromServer.Title + "</span>" + "<span class='year'>" + "Year: " + resultFromServer.Year + "</span>" + "<span class='rated'>" + "Rated: " + resultFromServer.Rated + "</span>" + "<span class='released'>" + "Released: " + resultFromServer.Released + "</span>" + "<span class='Genre'>" + "Genre: " + resultFromServer.Genre + "</span>" + "<span class='director'>" + "Director: " + resultFromServer.Director + "</span>" + "<span class='writer'>" + "Writer: " + resultFromServer.Writer + "</span>"
+    resultsInformation.innerHTML = "<div class='resultsInformation'>" + "<span class='title'>" + "Title: " + resultFromServer.Title + "</span>" + "<span class='year'>" + "Year: " + resultFromServer.Year + "</span>" + "<span class='rated'>" + "Rated: " + resultFromServer.Rated + "</span>" + "<span class='released'>" + "Released: " + resultFromServer.Released + "</span>" + "<span class='genre'>" + "Genre: " + resultFromServer.Genre + "</span>" + "<span class='director'>" + "Director: " + resultFromServer.Director + "</span>" + "<span class='writer'>" + "Writer: " + resultFromServer.Writer + "</span>"
+    for (var i = 0; i < resultFromServer.Ratings.length; i++) {
+        ratings += "<span class='source'>" + "Source: " + resultFromServer.Ratings[i].Source + "</span>";
+        ratings += "<span class='ratingsValue'>" + "Ratings: " + resultFromServer.Ratings[i].Value + "</span>";
+    }
+    ratings += "</div>"
+    document.getElementById('ratings').innerHTML = ratings;
 }
 
 document.getElementById('searchMovieTitleButton').addEventListener('click', () => {
